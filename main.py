ac302e766379991c436065481afc8c1a5d26ac1c
stripmola <stripmola@gmail.com>
2025-11-04 22:54:06 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! Welcome to the M-Pesa Bot 💰")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot is running...")
+    app.run_polling()
+
+if _name_ == "_main_":
+    main()
